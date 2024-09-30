ecc0773e21b55ab1c3fa2b776ea68222c700ebdf
TheKany <skarnjs06@naver.com>
2024-10-01 00:32:13 +0900

    프로젝트 타입 에러

--- components/_common/Card.tsx
+++ components/_common/Card.tsx
@@ -17,11 +17,24 @@ const Card = ({ children }: Props) => {
    * @param id id === 1 찬성 / id === 2 반대
    */
   const onClickVote = (id: string) => {
-    console.log(id);
+    if (process.env.NODE_ENV !== "production") {
+      // eslint-disable-next-line no-console
+      console.log(id);
+    }
   };
 
   return (
-    <div className="flip-container min-w-[104px] h-[154px] rounded-md p-1" onClick={onClickCard}>
+    <div
+      className="flip-container min-w-[104px] h-[154px] rounded-md p-1"
+      onClick={onClickCard}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          onClickCard();
+        }
+      }}
+      role="button"
+      tabIndex={0}
+    >
       <div className={`card ${isFlipped ? "flipped w-full h-full" : "w-full h-full"}`}>
         <div className="front w-full h-full text-center rounded-md shadow-md">
           <div className="voteEl">
@@ -29,8 +42,8 @@ const Card = ({ children }: Props) => {
           </div>
         </div>
         <div className="back w-full h-full relative p-2 rounded-md shadow-md flex items-end">
-          <div className="circle absolute left-2 top-2"></div>
-          <div className="circle absolute right-2 top-2"></div>
+          <div className="circle absolute left-2 top-2" />
+          <div className="circle absolute right-2 top-2" />
           <p className="card-backText absolute font-semibold text-2xl text-[#C65F2D] top-1/2 left-[48%] transform -translate-x-1/2 -translate-y-1/2">
             Trandy
           </p>
@@ -43,16 +56,18 @@ const Card = ({ children }: Props) => {
 
           <div className=" w-full grid grid-cols-2 gap-1">
             <button
+              type="button"
               onClick={() => onClickVote("1")}
               className="flex justify-center border border-white rounded-md py-1"
             >
-              <Image src={"./img/vote_yes.svg"} alt="" width={24} height={24} />
+              <Image src="./img/vote_yes.svg" alt="찬성버튼" width={24} height={24} />
             </button>
             <button
+              type="button"
               onClick={() => onClickVote("2")}
               className="flex justify-center border border-white rounded-md py-1"
             >
-              <Image src={"./img/vote_no.svg"} alt="" width={24} height={24} />
+              <Image src="./img/vote_no.svg" alt="반대버튼" width={24} height={24} />
             </button>
           </div>
         </div>
