17e3c9f6be29e7af8c22337cc2657311a74f1121
나도관 <9401ndk@t-win.kr>
2024-10-02 10:46:49 +0900

    Codedeploy, 배포 스크립트 추가

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 애플리케이션 디렉토리로 이동
+cd /path/to/your/nextjs/app
+
+# Next.js 애플리케이션을 백그라운드에서 실행
+echo "Next.js 애플리케이션 실행 중..."
+nohup npm run start > nextjs.log 2>&1 &
+
+echo "Next.js가 백그라운드에서 실행되었습니다."
