e8e1ac6cb89596d22d2b17d027d07d846f420e90
TheKany <skarnjs06@naver.com>
2024-10-03 00:19:08 +0900

    pwa 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
-// import withPWA from "next-pwa";
+import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pwa: {
+    dest: "public", // PWA 관련 파일들이 저장될 폴더를 설정 (public 폴더 사용)
+    register: true, // 서비스 워커 자동 등록
+    skipWaiting: true, // 페이지 새로고침 없이 새로운 서비스 워커를 활성화
+    disable: process.env.NODE_ENV === "development", // 개발 환경에서는 PWA 비활성화
+  },
+  env: {
+    SITE_URL: "https://trandy.site", // 사이트 URL을 환경 변수로 설정
+  },
+};
 
-// export default withPWA({
-//   ...nextConfig,
-//   dest: "build",
-// });
-
-export default nextConfig;
+export default withPWA(nextConfig);
